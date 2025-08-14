e6d7d90559c114a009892ebb3154c8b86aa75fe4
Ruthersmith Bercy <rbercy@five-startech.com>
2025-08-14 08:48:19 -0400

    added provider since could be required

--- module/terraform-aws-ecs-service/main.tf
+++ module/terraform-aws-ecs-service/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+
 variable "service_name" {
     description = "Name for the service"
 }
